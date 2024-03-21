da42aa6c7397f2b266356b1e076017b52a883329
Chim chikwakwa <chimwec98@gmail.com>
2024-03-21 22:57:26 +0100

    another good one

--- games/models.py
+++ games/models.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 
 # Create your models here.
 class UserProfile(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None, null=True)
     bio = models.TextField(max_length=500, blank=True)
     birthday = models.DateField(null=True, blank=True)
 
--- games/views.py
+++ games/views.py
@@ -66,7 +66,7 @@ class CustomLoginView(LoginView):
 
 def logout_view(request):
     logout(request)
-    return redirect('home.html')
+    return redirect('home')
 
 
 class UserProfile(CreateView):
