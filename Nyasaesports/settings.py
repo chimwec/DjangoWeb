52926fa89dd8137b649b92b144bd4bfee57ab862
Chim chikwakwa <chimwec98@gmail.com>
2024-10-15 20:51:30 +0200

    added some security

--- Nyasaesports/settings.py
+++ Nyasaesports/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-oz$2192bq)ghxddv*haxb1fw7%!=a_en9btfc@r(2=u6jczzmk'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
